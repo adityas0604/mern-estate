fa8da98e5a5bb9e797507c1479b2a9edc08b341d
adityas0604 <dharmaa130@gmail.com>
2024-06-11 21:09:04 +0530

    sign-up ui done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ export default function
     <Routes>
       <Route path = "/" element = {<Home/>}> </Route>
       <Route path = "/sign-in" element = {<SignIn/>}> </Route>
-      <Route path = "/sing-up" element = {<SignUp/>}> </Route>
+      <Route path = "/sign-up" element = {<SignUp/>}> </Route>
       <Route path = "/about" element = {<About/>}> </Route>
       <Route path = "/profile" element = {<Profile/>}> </Route>
     </Routes>
