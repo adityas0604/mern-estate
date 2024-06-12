663bce24ab82aa4a79d58fdbf472b22d4b85f1a1
adityas0604 <dharmaa130@gmail.com>
2024-06-12 13:23:45 +0530

    complete sign-in page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const signin = async (req, res, next) => {
     }
     const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
     const {password:pass, ...rest} = user._doc
-    res.cookie('token', token, {httpOnly:true}).status(200).json(rest)
+    res.cookie('access_token', token, {httpOnly:true}).status(200).json(rest)
 
     } catch (err) {
         next(err)
